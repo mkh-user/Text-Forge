76a94fbc941f150d8200b39127da399ee0d4e35d
Mahan <khalili1388mahan@gmail.com>
2025-07-03 21:32:52 +0330

    Fix "Reset Font Size" action
    In fe826a3e39152050d0e3776c07762aff4049a10f

--- scripts/reset_font_size.gd
+++ scripts/reset_font_size.gd
@@ -1,7 +1,9 @@
 extends ActionScript
 
 func _ready() -> void:
+	_load_shortcut()
 	Global.get_editor().add_theme_font_size_override("font_size", Settings.get_setting("editor_ui", "font_size", 16))
 
 func _run_action() -> void:
 	Global.get_editor().add_theme_font_size_override("font_size", Settings.get_setting("editor_ui", "base_font_size", 16))
+	Settings.set_setting("editor_ui", "font_size", Settings.get_setting("editor_ui", "base_font_size", 16))
